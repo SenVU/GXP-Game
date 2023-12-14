f43750f5b2336b564b87e6c7ec1aaec160d8bcef
Sen VU <548068@student.saxion.nl>
2023-12-14 14:17:56 +0100

    added tilemap

---
+++ GXPEngine/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiledMapParser;
+
+class LevelManager : EasyDraw
+{
+    Level currentLevel;
+    public LevelManager() : base (0,0){
+        
+    }
+
+    public void LoadLevel (Level level) {
+        
+    }
+}
